a9deeb806b6344c47b682ad853c893cd0c924597
Rifat Alfasya <rifatalfasya.eng@gmail.com>
2026-02-16 04:15:10 +0800

    third commit

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
-from mangum import Mangum
-
 from data import kamus
 from cors import origins
 
@@ -34,4 +32,7 @@ async def get_kata(kata):
             return k
     return { "error" : "kata tidak ditemukan." }
 
-handler = Mangum(app)
+if __name__ == "__main__":
+    import uvicorn
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("main:app", host="0.0.0.0", port=port)
