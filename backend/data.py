e207ebf1b60eba1196408301d0477bb4ba80fe63
Rifat Alfasya <rifatalfasya.eng@gmail.com>
2026-02-16 02:56:56 +0800

    Push all files to GitHub

---
+++ backend/data.py
@@ -0,0 +1,4 @@
+kamus = [
+    { "id" : 1, "kata" : "teknologi", "definisi" : "alat yang mempermudah pekerjaan manusia." },
+    { "id" : 2, "kata" : "inovasi", "definisi" : "sesuatu yang merubah bagaimana dunia bekerja." },
+]
---
+++ frontend/app.js
@@ -0,0 +1,23 @@
+const showButton = document.getElementById("show-button");
+const kamusContainer = document.getElementById("kamus-container");
+const listKamus = document.getElementById("list-kamus");
+
+showButton.addEventListener("click", showKamus);
+
+async function showKamus() {
+    const response = await fetch("http://127.0.0.1:8000/kamus")
+    const data = await response.json();
+    for (i=0; i < data.length; i++) {
+        const newElement = document.createElement("li");
+        newElement.textContent = `${data[i].kata}: ${data[i].definisi}`
+        listKamus.appendChild(newElement);
+    }
+    showButton.textContent = "Sembunyikan";
+    showButton.addEventListener("click", hideKamus);
+}
+
+async function hideKamus() {
+    kamusContainer.textContent = ""
+    showButton.textContent = "Tampilkan";
+    showButton.addEventListener("click", showKamus);
+}
