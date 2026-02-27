70cc5c6e9b959b354f98774bdf64e19ca6ab44d4
Rifat Alfasya <rifatalfasya.eng@gmail.com>
2026-02-27 23:32:39 +0800

    first commit

--- backend/routes/kamus.router.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-
-import { getKata } from "../controllers/getkata.controller.js";
-
-const kamusRouter = Router();
-
-kamusRouter.post("/:kata", getKata);
-
-export { kamusRouter }
