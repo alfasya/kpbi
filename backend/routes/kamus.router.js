4f7b0b2249fb08f52d4037ad4e1fee6facd4c6a0
Rifat Alfasya <rifatalfasya.eng@gmail.com>
2026-02-28 06:16:07 +0800

    add home endpoint

--- backend/routes/kamus.router.js
+++ backend/routes/kamus.router.js
@@ -4,6 +4,10 @@ import { cariKata } from '../controllers/kamus.controller.js';
 
 const kamusRouter = Router();
 
+kamusRouter.get('/', (req, res)=> {
+    res.status(200).json({ message: 'Hello' });
+});
+
 kamusRouter.get('/:kata', cariKata);
 
 export { kamusRouter }
