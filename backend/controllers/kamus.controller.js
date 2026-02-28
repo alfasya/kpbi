0d9aa898b60af42f51a1810fb0375aa7d1b8078b
Rifat Alfasya <rifatalfasya.eng@gmail.com>
2026-03-01 02:47:11 +0800

    add string validator

--- backend/controllers/kamus.controller.js
+++ backend/controllers/kamus.controller.js
@@ -1,10 +1,22 @@
 import { queryKBBI } from "../db/query.db.js";
 
 async function cariKata(req, res) {
-    const kata = req.params.kata;
+    const kata = req.params.kata.trim().toLowerCase();
+
+    if (typeof(kata) != 'string' || kata.length >= 50) {
+        return res.status(400).json({
+            message: 'Masukkan kata yang sesuai.'
+        });
+    }
 
     try {
         const result = await queryKBBI(kata);
+
+        if (result.length === 0) {
+            return res.status(404).json({
+                message: 'Kata tidak ditemukan.'
+            });
+        }
 
         res.status(200).json({
             message: result
