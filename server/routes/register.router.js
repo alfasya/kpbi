22a932f0a3b80b6549f78cc37965d297081dec33
Rifat Alfasya <rifatalfasya.eng@gmail.com>
2026-04-01 11:28:27 +0800

    deleted unnecessary files... for now

--- server/routes/register.router.js
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-
-import { registerHandler } from '../controllers/register.controller.js';
-
-const registerRouter = Router();
-
-registerRouter.post('/', registerHandler);
-
-export { registerRouter }
