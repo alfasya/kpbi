7b53b55f2cd69e071b836c2b8af69bd2905aabf7
Rifat Alfasya <rifatalfasya.eng@gmail.com>
2026-04-11 21:13:56 +0800

    enhancing element styles and layouts

--- client/app.js
+++ client/app.js
@@ -1,5 +1,6 @@
 const searchBar = document.getElementById('search-bar')
 const searchButton = document.getElementById('search-button')
+const content = document.getElementById('content')
 const kbbi = document.getElementById("kbbi")
 const llm = document.getElementById("llm")
 
@@ -17,6 +18,7 @@ async function getFunc(e) {
     const data = await request.json()
 
     const kbbiList = document.createElement("ol")
+    kbbiList.classList.add('kata')
     kbbi.appendChild(kbbiList)
 
     for (let i=0;i<data.message.definisi.length;i++) {
@@ -28,6 +30,7 @@ async function getFunc(e) {
         kbbiList.appendChild(definisi)
 
         const keterangan = document.createElement('ul')
+        keterangan.classList.add('keterangan')
         definisi.appendChild(keterangan)
 
         for (let v in data.message.definisi[i]) {
@@ -45,20 +48,25 @@ async function getFunc(e) {
     kbbiNote.textContent = "Bersumber dari KBBI"
     kbbi.appendChild(kbbiNote)
 
-    const p = document.createElement('p')
-    llm.classList.add('llm-style')
-    p.textContent = 'Generating text...'
-    llm.appendChild(p)
+    const loading = document.createElement('p')
+    llm.classList.add('loading')
+    loading.textContent = 'Generating text...'
+    llm.appendChild(loading)
 
     const response = await fetch(`http://localhost:3000/api/kamus/generate/${kata}`)
     const generatedData = await response.json()
 
     llm.replaceChildren()
 
+    const textList = document.createElement('ol');
+    textList.classList.add('text-list')
+    llm.appendChild(textList)
+
     for (let i=0;i<generatedData.response.length;i++) {
-        const p = document.createElement('p')
-        p.textContent = generatedData.response[i]
-        llm.appendChild(p)
+        const text = document.createElement('p')
+        text.classList.add('text')
+        text.textContent = generatedData.response[i]
+        textList.appendChild(text)
     }
 
     const llmNote = document.createElement('p')
