85f783d8039699e8b1033f4409daa9e7e52147c2
Rifat Alfasya <rifatalfasya.eng@gmail.com>
2026-04-14 04:36:26 +0800

    edit ollama configuration

--- server/ollama.js
+++ server/ollama.js
@@ -3,14 +3,17 @@ import ollama from 'ollama';
 async function llmResponse(kata) {
     const PROMPT = `You are article writer. List three examples of sentence using this word: ${kata} in Indonesian language, without introduction, no english.`;
     const response = await ollama.chat({
-        model: 'qwen3.5:0.8b',
+        model: 'qwen3:0.6b',
         messages: [{ role: 'user', content: PROMPT }],
         stream: false
     });
 
-    const resArr = response.message.content.split('\n');
+    const res = {
+        model: response.model,
+        response: response.message.content.split('\n'),
+    }
 
-    return resArr;
+    return res
 }
 
 export { llmResponse };
